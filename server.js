155b27eb95355771cff810ece1d046a6b3184b27
@Marcohiro <petit.marcalain@yahoo.fr>
2020-06-13 01:36:01 +0200

    Fixed import package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var router = express.Router();
 const parser = require('./modules/chunkParser');
 const app = express();
-const connect = require('.RabbitMQ/publisher');
+const connect = require('./RabbitMQ/publisher');
 
 app.get("/queue", function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text plain' });
