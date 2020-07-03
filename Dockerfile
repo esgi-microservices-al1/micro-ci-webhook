bf479709201b516996e0e088721cf313a7f6e857
Pierre Sid-idris <pierre.sididris@live.fr>
2020-07-03 11:24:09 +0200

    fix global path

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:10
 WORKDIR /usr/src/app
 COPY node/package*.json ./
 RUN npm install
-COPY ./node ./
+COPY . .
 
 EXPOSE 8082
 CMD [ "npm", "start" ]
--- node/constants/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    githubClean : ['tree_id', 'distinct', 'url', 'committer'],
-    githubCleanCommiter : ['name'],
-    gitlabClean : ['title', 'url'],
-    bitbucketClean : ['rendered', 'links', 'parents', 'type', 'properties'],
-});
