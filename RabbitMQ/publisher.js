ddf69b460b8f9df4a12e507c52693a92b30bc3d9
Amine <elbaraka.amine2@gmail.com>
2020-07-10 22:51:37 +0200

    fixed templating with env on rabbitmq

--- RabbitMQ/publisher.js
+++ RabbitMQ/publisher.js
@@ -1,20 +1,24 @@
-const amqp = require("amqplib");
+const dotenv = require('dotenv').config();
 
+const amqp = require("amqplib").credentials.plain(process.env.BROKER_LOGIN, process.env.BROKER_PASSWORD);
+
+console.log(`amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`);
+console.log(process.env.RABBITMQ_HOST);
 
 var connect = async function (arg) {
     try {
         //creates a connection to RabbitMQ server on docker which is running on port 5672
         //and creates a channel to communicate
         // const connection = await amqp.connect("amqp://localhost:5672");
-        const connection = await amqp.connect("amqp://10.0.0.5:40701");
+        const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`);
         const channel = await connection.createChannel();
         
         //asserts a message queue exists and creates it if it doesn't 
-        const result = await channel.assertQueue("jobs"); 
+        const result = await channel.assertQueue(process.env.BROKER_QUEUE); 
 
         //sends the message to the queue that we created called "Jobs"
         console.log(arg);
-        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(arg)));
+        channel.sendToQueue(process.env.BROKER_QUEUE, Buffer.from(JSON.stringify(arg)));
         
         console.log(`job sent succefully`);
     } catch (ex) {
