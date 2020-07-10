b7f443ea8cbbf78491d7ff3f7bd200aed87f7bf4
Pierre Sid-idris <pierre.sididris@live.fr>
2020-07-10 22:14:42 +0200

    add traefik tag

--- consul/consul.js
+++ consul/consul.js
@@ -25,7 +25,10 @@ let details = {
         deregister_critical_service_after: '1m'
     },
     tags : [
-        'Webhook',
+        // 'Webhook',
+        'traefik.enable=true',
+        'traefik.frontend.entryPoints=http',
+        'traefik.frontend.rule=PathPrefixStrip:/webhook/'
     ],
     token: process.env.TOKEN
 };
